63030b90ae29d5ac75a6c9bd9b415030cf5c5c5c
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-02-28 20:54:53 +0530

    learning recursion

--- java-datastructure/src/Recursion/Quetions.md
+++ java-datastructure/src/Recursion/Quetions.md
@@ -1,12 +1,32 @@
 ## QUETIONS
 
-# 1. Tower of HANOI
+# 1. Tower of HANOI.
 `1. only one disk is transfered to the destination at a time`   
 `2. smaller disk is placed on the larger disk `
 `time complexity O(2^n)`
 
-# 2. PRINT A STRING IN REVERSE
+# 2. PRINT A STRING IN REVERSE.
+`time complexity O(n)`
 
-# 3. FIND THE FIRST AND LAST OCCURANCE OF THE CHAR IN THE ELEMENT
+# 3. FIND THE FIRST AND LAST OCCURANCE OF THE CHAR IN THE ELEMENT.
+`time complexity O(n)`
 
-# 4. Check if array is sorted (strictly increasing)
+# 4. Check if array is sorted (strictly increasing).
+`time complexity O(n)`
+
+# 5. Move all x to the end of the string.
+`time complexity O(n)`
+
+# 6. Remove Duplicates in a String
+`time complexity O(n)`
+
+# 7. print all the subsesequence of a string (!important)
+`time complexity O(2^n)`
+
+# 8. Print all the unique subsesequence of a string
+`time complexity O(2^n)`
+
+# 9. Print keypad combinations
+`time complexity O(4^n)`
+
+
--- java-datastructure/src/Recursion/RecursionJava.java
+++ java-datastructure/src/Recursion/RecursionJava.java
@@ -1,5 +1,8 @@
 package Recursion;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class RecursionJava {
 
     // 1 ans
@@ -45,7 +48,118 @@ public class RecursionJava {
         }
         findFirstLastOcc(str, idx+1, element);
     }
+
+    //ans 4
+    public static boolean isSorted(int arr[],int idx){
+        // base case
+        if(idx == arr.length - 1){
+            return true;
+        }
+
+        if(arr[idx] >= arr[idx+1]){
+            return false;
+        }
+
+        return isSorted(arr, idx+1);
+    } 
+
+    // ans 5
+    public static void Moveallx(String str,int idx,String newString , int count){
+            if(idx == str.length()){
+                for(int i=0;i<count;i++){
+                    newString += 'x';
+                }
+                System.out.println(newString);
+                return;
+            }
+
+            if(str.charAt(idx) == 'x'){
+                count++;
+                Moveallx(str, idx+1, newString, count);
+            }
+            else{
+                newString += str.charAt(idx);
+                Moveallx(str, idx+1, newString, count);
+            }
+    }
     
+    // 6 ans
+    public static boolean[] map = new boolean[26];
+
+    public static void RemoveDuplicats(String str,String newString,int idx){
+
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currchar = str.charAt(idx);
+        if(map[currchar - 'a']) // return if 'a'-'a' = 0 , 'b'-'a' = 1
+        {
+            // for true case
+            RemoveDuplicats(str, newString, idx+1);
+        }
+        else{
+            newString += currchar;
+            map[currchar - 'a'] = true;
+            RemoveDuplicats(str, newString, idx+1);
+        }
+    }
+
+    // ans 7
+    public static void SubSequences(String str,int idx , String newString){
+
+        if(str.length() == idx){
+            System.out.println(newString);
+            return;
+        }
+
+        char currchar = str.charAt(idx);
+
+        SubSequences(str, idx+1, newString+currchar);
+
+        SubSequences(str, idx+1, newString);
+    }
+
+    // ans 8
+    public static void SubSequenceswithnorep(String str,int idx , String newString,HashSet<String> set){
+        
+        if(str.length() == idx){
+            if(set.contains(newString)){
+                return;
+            }
+            else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+
+        char currchar = str.charAt(idx);
+
+
+        SubSequenceswithnorep(str, idx+1, newString+currchar, set);
+
+        SubSequenceswithnorep(str, idx+1, newString, set);
+    }
+
+    // ans 9 
+    public static String mapping[] = {".","abc","def","jkl","mno","pqrs","tu","vwx","yz"};
+    public static void KeypadComb(String str,int idx,String combination){
+
+        if(idx == str.length()){
+            System.out.println(combination);
+            return;
+        }
+
+        char currchar = str.charAt(idx);
+        String mapp = mapping[currchar - '0'];
+
+        for(int i=0;i<mapp.length();i++){
+            char s = mapp.charAt(i);
+            KeypadComb(str, idx+1, combination+s);
+        }
+    }
 
     public static void main(String[] args) {
         int n = 3;
@@ -57,5 +171,23 @@ public class RecursionJava {
         String str1 = "abaaabcdaanfaadf";
         findFirstLastOcc(str1, 0, 'a');
        
+        int arr[] = {1,2,3,4};
+        System.out.println(isSorted(arr, 0));
+
+        String str2 = "axbdxbxss";
+        Moveallx(str2, 0, "", 0);
+
+        String str3 = "aabbcdeerrsw";
+        RemoveDuplicats(str3,"", 0);
+
+        String str4 = "abc";
+        SubSequences(str4, 0, "");
+
+        String str5 = "aaa";
+        HashSet<String> set = new HashSet<String>();
+        SubSequenceswithnorep(str5, 0, "",set);
+
+        String str6 = "23";
+        KeypadComb(str6, 0, "");
     }
 }
